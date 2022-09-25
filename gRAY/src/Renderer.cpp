26e2adc87fe3f34f534b94b466a9a0a11b312331
Googli <jorjikiaguga@gmail.com>
2022-09-25 15:32:26 +0400

    Added lighting and normals

--- gRAY/src/Renderer.cpp
+++ gRAY/src/Renderer.cpp
@@ -1,6 +1,19 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+namespace Utils {
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -31,21 +44,25 @@ void Renderer::Render()
 			//0, 1 -> -1, 1
 			coord = coord * 2.0f - 1.0f;
 
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
 	m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
 
 	float radius = 0.5f;
-	glm::vec3 rayOrigin(0.0f, 0.0f,  2.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	glm::vec3 sphereColor(0, 1, 0);
+	glm::vec3 lightDirection = glm::normalize(glm::vec3(-1,-1,-1));
 	//rayDirection = glm::normalize(rayDirection);
 
 	// (bx^2 + by^2 + bz^2)t^2 + (2(axbx + ayby + azbz)t + (ax^2 + ay^2 - r^2) = 0
@@ -60,11 +77,22 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	float cTerm = glm::dot(rayOrigin, rayOrigin) - radius * radius;
 
 	// Quadratic
-	// b^2 -4ac
+	// b^2 - 4ac
 
 	float D = bTerm * bTerm - 4.0f * aTerm * cTerm;
+	if (D <= 0.0f)
+		return glm::vec4(0, 0, 0, 1);
+	
+	// (-b +- sqrt(D)) / 2a
+	//float root0 = (-bTerm + glm::sqrt(D)) / (2.0f * aTerm);
+	float root1 = (-bTerm - glm::sqrt(D)) / (2.0f * aTerm); //closest
 
-	if (D >= 0.0f)
-		return 0xff00ff00;
-	return 0xffffffff;
+	//glm::vec3 hit1 = rayOrigin + rayDirection * root0;
+	glm::vec3 hit2 = rayOrigin + rayDirection * root1;
+	glm::vec3 normal = glm::normalize(hit2);
+
+	float angle = glm::max(glm::dot(normal, -lightDirection), 0.0f);
+
+	sphereColor *= angle;
+	return glm::vec4(sphereColor, 1.0f);
 }
